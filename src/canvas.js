9d09a1fb43643a336c497455a92029e94eb3368b
wcorderolara <wcordero.lara@gmail.com>
2019-08-30 11:57:01 -0600

    change fillRect

--- src/canvas.js
+++ src/canvas.js
@@ -11,7 +11,7 @@ export class GameCanvas {
         canvasElement.setAttribute('width', VIEWPORT_WIDTH * SCREEN_VIEWPORT_RATIO);
 
         this.context.fillStyle = '#e2fcbf';
-        this.context.fillRect = (0, 0, canvasElement.clientWidth, canvasElement.clientHeight);
+        this.context.fillRect(0, 0, this.canvasElement.clientWidth, this.canvasElement.clientHeight);
     }
 
     drawImage(image, x, y, width, height) {
