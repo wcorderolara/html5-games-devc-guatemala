829df39bba223f8cb51d2ba4c967cf520da42135
wcorderolara <wcordero.lara@gmail.com>
2019-08-30 12:02:21 -0600

    fixes

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ canvas.setAttribute('width', VIEWPORT_WIDTH * SCREEN_VIEWPORT_RATIO);
 assets.loadImage('../images/Green_Blob.png').then( img => {
     // Drawing a Background
     context.fillStyle = '#e2fcbf';
-    context.fillRect = (0, 0, canvas.clientWidth, canvas.clientHeight);
+    context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
     // context.drawImage(img, 25, 25);
     context.drawImage(img, 25 * SCREEN_VIEWPORT_RATIO, 25 * SCREEN_VIEWPORT_RATIO, 
                       128 * SCREEN_VIEWPORT_RATIO, 128 * SCREEN_VIEWPORT_RATIO);
